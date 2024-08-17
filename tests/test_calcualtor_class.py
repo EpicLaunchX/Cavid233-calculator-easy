1d7db02c41b161d47d49acd175520933309df42e
Cavid233 <salmanov.cavid1@gmail.com>
2024-08-17 12:29:40 +0400

    feat: add Caluclator class and test covarage

---
+++ tests/test_calcualtor_class.py
@@ -0,0 +1,28 @@
+import pytest
+
+from src.pytemplate.domain.models import Operands
+from src.pytemplate.service.calculator import Calculator
+
+
+def test_add_function():
+    operands = Operands(5, 6)
+
+    assert Calculator().add(operands) == 11
+
+
+def test_subtract_function():
+    operands = Operands(5, 6)
+
+    assert Calculator().subtract(operands) == -1
+
+
+def test_multiply_function():
+    operands = Operands(5, 6)
+
+    assert Calculator().multiply(operands) == 30
+
+
+def test_divide_function():
+    operands = Operands(35, 6)
+
+    assert Calculator().divide(operands) == 5
